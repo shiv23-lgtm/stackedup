7581cad53ef4f8a3d104a3afce72c087098aa2a7
shiv23-lgtm <shiv23@gmail.com>
2026-02-28 16:44:46 -0500

    Add files via upload

---
+++ accounts.js
@@ -0,0 +1,19 @@
+const { PlaidApi, PlaidEnvironments, Configuration } = require('plaid');
+
+const client = new PlaidApi(new Configuration({
+  basePath: PlaidEnvironments[process.env.PLAID_ENV || 'development'],
+  baseOptions: { headers: { 'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID, 'PLAID-SECRET': process.env.PLAID_SECRET } },
+}));
+
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+  try {
+    const { data } = await client.itemPublicTokenExchange({ public_token: req.body.public_token });
+    res.json({ access_token: data.access_token });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
